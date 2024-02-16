c1403374eb4581c9d707b8b09c7fa0beac9f2c7c
iamDaniel <idowudanielcsc190400@gmail.com>
2024-02-16 21:19:17 +0100

    worked on imagepi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:contactapp/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  await Hive.openBox('contact');
+
   runApp(const MyApp());
 }
 
--- lib/pages/new_contact.dart
+++ lib/pages/new_contact.dart
@@ -1,5 +1,9 @@
+import 'dart:html';
+import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:contactapp/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class new_in extends StatefulWidget {
   const new_in({super.key});
@@ -9,6 +13,7 @@ class new_in extends StatefulWidget {
 }
 
 class _new_inState extends State<new_in> {
+  File? imageFile;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,13 +54,41 @@ class _new_inState extends State<new_in> {
                   Center(
                     child: Stack(
                       children: [
-                        CircleAvatar(
-                          radius: 70,
-                        ),
+                        imageFile == null
+                            ? Image.asset(
+                                'asset/images/user default image.png',
+                                height: 100,
+                                width: 100,
+                              )
+                            : ClipRRect(
+                                borderRadius: BorderRadius.circular(150),
+                                child: Image.file(
+                                  imageFile!,
+                                  height: 100,
+                                  width: 100,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
                         Positioned(
                           child: IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.add_a_photo,color: Colors.black, size: 30,),
+                            onPressed: () async {
+                              Map<Permission, PermissionStatus> statuses =
+                                  await [
+                                Permission.storage,
+                                Permission.camera,
+                              ].request();
+                              if (statuses[Permission.storage]!.isGranted &&
+                                  statuses[Permission.camera]!.isGranted) {
+                                showImagePicker(context);
+                              } else {
+                                AlertDialog();
+                              }
+                            },
+                            icon: Icon(
+                              Icons.add_a_photo,
+                              color: Colors.black,
+                              size: 30,
+                            ),
                           ),
                           bottom: -10,
                           left: 80,
@@ -73,7 +106,6 @@ class _new_inState extends State<new_in> {
                         decoration: InputDecoration(
                             labelText: "Full Name",
                             hintStyle: TextStyle(color: Colors.amber),
-                            suffixIcon: Icon(Icons.mail),
                             filled: true,
                             border: InputBorder.none),
                         onChanged: (value) {},
@@ -232,4 +264,108 @@ class _new_inState extends State<new_in> {
       ),
     );
   }
+
+  final picker = ImagePicker();
+
+  void showImagePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Container(
+        height: 200,
+        margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Uplode an Image',
+              style: TextStyle(fontSize: 25),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          _imgFromCamera();
+                        },
+                        icon: Icon(
+                          Icons.camera,
+                          size: 60,
+                        ),
+                      ),
+                      Text(
+                        'Camera',
+                        style: TextStyle(fontSize: 18),
+                      )
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          _imgFromGallery();
+                        },
+                        icon: Icon(
+                          Icons.image,
+                          size: 60,
+                        ),
+                      ),
+                      Text(
+                        'Gallery',
+                        style: TextStyle(fontSize: 18),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _imgFromGallery() async {
+    await picker
+        .pickImage(
+      source: ImageSource.gallery,
+    )
+        .then((value) {
+      if (value != null) {
+        (value.path);
+      }
+    });
+  }
+
+  _imgFromCamera() async {
+    await picker
+        .pickImage(
+      source: ImageSource.camera,
+    )
+        .then((value) {
+      if (value != null) {
+        (value.path);
+      }
+    });
+  }
+
+  // void takePhoto(ImageSource source) async {
+  //   final PickedFile = await _picker.getImage(
+  //     source: source,
+  //   );
+  //   setState(() {
+  //     _imageFile = PickedFile;
+  //   });
+  // }
 }
